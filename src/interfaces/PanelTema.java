bc3b7bca4a18ffa0da7a586f6988ba29bb9c7f70
cmarti0747 <carlosmartiglez@hotmail.com>
2017-05-05 20:27:32 +0200

    x

--- src/interfaces/PanelTema.java
+++ src/interfaces/PanelTema.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JList;
@@ -16,13 +17,13 @@ public class PanelTema extends JPanel{
 	private JList apuntes, ejercicios;
 	private JLabel nombreTema, labelapuntes, labelejercicios;
 	private JButton crearA, eliminarA, crearE, eliminarE;
-	
+	private JButton crearSub;
+	private JButton eliminarSub;
+	private DefaultListModel<String> lApuntes,lEjercicios;
+	private DefaultListModel<String> lSubtemas;
+	private JList subtemas;
 	
 	public PanelTema(){
-		
-
-		String[] nombres = {"Ana","Margarita","Daniela","Divian", 
-		"Leslie","Paz","Andrea","Macarena"}; 
 		
 		nombreTema = new JLabel();
 		nombreTema.setFont(new Font("Arial",0,40));
@@ -38,23 +39,33 @@ public class PanelTema extends JPanel{
 		crearE = new JButton("Crear Ejercicio");
 		eliminarE = new JButton("Eliminar Ejercicio");
 		
+		crearSub = new JButton("Crear Subtema");
+		eliminarSub = new JButton("Eliminar Subtema");
 		
-		
-		
-		apuntes = new JList(nombres);
+		lApuntes = new DefaultListModel<String>();
+		apuntes = new JList(lApuntes);
 		apuntes.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION); 
 		JScrollPane barraDesplazamiento = new JScrollPane(apuntes); 
 		barraDesplazamiento.setPreferredSize(new Dimension(250,250));
 		barraDesplazamiento.getViewport().setBackground(Color.WHITE);
 		apuntes.setFont(new Font("Arial",0,30));
 		
-		ejercicios = new JList(nombres);
+		lEjercicios = new DefaultListModel<String>();
+		ejercicios = new JList(lEjercicios);
 		ejercicios.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION); 
 		JScrollPane barraDesplazamiento2 = new JScrollPane(ejercicios); 
 		barraDesplazamiento2.setPreferredSize(new Dimension(250,250));
 		barraDesplazamiento2.getViewport().setBackground(Color.WHITE);
 		ejercicios.setFont(new Font("Arial",0,30));
 		
+		
+		lSubtemas = new DefaultListModel<String>();
+		subtemas = new JList(lSubtemas);
+		subtemas.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION); 
+		JScrollPane barraDesplazamiento3 = new JScrollPane(subtemas); 
+		barraDesplazamiento3.setPreferredSize(new Dimension(250,250));
+		barraDesplazamiento3.getViewport().setBackground(Color.WHITE);
+		subtemas.setFont(new Font("Arial",0,30));
 		SpringLayout layout = new SpringLayout();
 		setLayout(layout);
 		
@@ -75,16 +86,20 @@ layout.putConstraint(SpringLayout.NORTH, labelejercicios, 25, SpringLayout.SOUTH
 layout.putConstraint(SpringLayout.WEST, labelejercicios, 450, SpringLayout.WEST, nombreTema);
 		
 		
-layout.putConstraint(SpringLayout.NORTH, barraDesplazamiento2, 67, SpringLayout.SOUTH, nombreTema);
+layout.putConstraint(SpringLayout.NORTH, barraDesplazamiento2, 70, SpringLayout.SOUTH, nombreTema);
 		
-		layout.putConstraint(SpringLayout.WEST, barraDesplazamiento2, 450, SpringLayout.WEST, nombreTema);
+		layout.putConstraint(SpringLayout.WEST, barraDesplazamiento2, 325, SpringLayout.WEST, nombreTema);
 		
+layout.putConstraint(SpringLayout.NORTH, barraDesplazamiento3, 70, SpringLayout.SOUTH, nombreTema);
 		
+		layout.putConstraint(SpringLayout.WEST, barraDesplazamiento3, 650, SpringLayout.WEST, nombreTema);
+		
+			
 		
 		
 layout.putConstraint(SpringLayout.NORTH, crearA, 35, SpringLayout.SOUTH, barraDesplazamiento);
 		
-		layout.putConstraint(SpringLayout.WEST, crearA, 5, SpringLayout.WEST, barraDesplazamiento);
+		layout.putConstraint(SpringLayout.WEST, crearA, -5, SpringLayout.WEST, barraDesplazamiento);
 		
 layout.putConstraint(SpringLayout.NORTH, eliminarA, 0, SpringLayout.NORTH, crearA);
 		
@@ -96,23 +111,35 @@ layout.putConstraint(SpringLayout.NORTH, eliminarA, 0, SpringLayout.NORTH, crear
 		
 layout.putConstraint(SpringLayout.NORTH, crearE, 35, SpringLayout.SOUTH, barraDesplazamiento2);
 		
-		layout.putConstraint(SpringLayout.WEST, crearE, 5, SpringLayout.WEST, barraDesplazamiento2);
+		layout.putConstraint(SpringLayout.WEST, crearE, -5, SpringLayout.WEST, barraDesplazamiento2);
 		
 layout.putConstraint(SpringLayout.NORTH, eliminarE, 0, SpringLayout.NORTH, crearE);
 		
 		layout.putConstraint(SpringLayout.WEST, eliminarE, 10, SpringLayout.EAST, crearE);
 		
 		
+layout.putConstraint(SpringLayout.NORTH, crearSub, 35, SpringLayout.SOUTH, barraDesplazamiento3);
+		
+		layout.putConstraint(SpringLayout.WEST, crearSub,-5, SpringLayout.WEST, barraDesplazamiento3);
+		
+layout.putConstraint(SpringLayout.NORTH, eliminarSub, 0, SpringLayout.NORTH, crearSub);
+		
+		layout.putConstraint(SpringLayout.WEST, eliminarSub, 10, SpringLayout.EAST, crearSub);
+		
+		
+
 
 		
 		add(barraDesplazamiento);
 		add(barraDesplazamiento2);
+		add(barraDesplazamiento3);
 		add(crearA);
 		add(eliminarA);
 		
 		add(crearE);
 		add(eliminarE);
-		
+		add(crearSub);
+		add(eliminarSub);
 		
 add(labelapuntes);
 add(labelejercicios);
